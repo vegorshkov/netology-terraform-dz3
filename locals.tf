28ce257370aedc93b0f26667394449b22bcad9dd
Горшков Виктор Евгеньевич <vgorshkov@gid.ru>
2026-02-06 23:11:21 +0300

    Задание 7

--- locals.tf
+++ locals.tf
@@ -1,4 +1,12 @@
 locals {
   ssh_public_key = var.ssh_public_key
   nat_enable_ext_ip = true
+
+  vpc = {
+    network_id = "enp7i560tb28nageq0cc"
+    subnet_ids = ["e9b0le401619ngf4h68n", "e2lbar6u8b2ftd7f5hia", "b0ca48coorjjq93u36pl", "fl8ner8rjsio6rcpcf0h"]
+    subnet_zones = ["ru-central1-a", "ru-central1-b", "ru-central1-c", "ru-central1-d"]
+  }
+
 }
+
